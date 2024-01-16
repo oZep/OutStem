c8576f5dba6c587bac6ab4fa7d1920102295eaf5
joeyissa <joeyissa59@gmail.com>
2024-01-15 23:01:09 -0500

    thinking through 3rd task because i want to prove i can do it in O(n)

--- slice-of-pi-dashboard/src/components/TotalDisplay.js
+++ slice-of-pi-dashboard/src/components/TotalDisplay.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CountUpToggle from './CountUpToggle';
-//import PricingData from '../data/pricing_data.json';
+import PricingData from '../data/pricing_data.json';
 import OrderData from '../data/order_data.json';
 
 
@@ -13,23 +13,27 @@ const TotalDisplay = () => {
 
     OrderData.forEach((order) => {  
         const store = order.store; 
-        const type = order.items.type;
-        const size = order.items.zie;
+        const type = order.items[0].type;
+        const size = order.items[0].zie;
 
         // try directly matching to the same values in the other json file 
         if (!ordersPlaced[store]) {  
             ordersPlaced[store] = {}; 
         } 
-        
-        if (!ordersPlaced[store][order.items.type]) {
-            ordersPlaced[store][order.items.size] = 1;
+        if (!ordersPlaced[store][type]) {
+            ordersPlaced[store][type] = {};
+        }
+        if (!ordersPlaced[store][type][size]) {
+            ordersPlaced[store][type][size] = 1;
         }
         else {
-            ordersPlaced[store][order.items.size] += 1;
+            ordersPlaced[store][type][size] += 1;
         }
     });
 
-    // Organized like {Store: {"L": , "W": }}
+    
+
+    // Organized like {Store: {"Cheese": {"S": 3, "L": 2}, "Pepperoni": {"
 
     console.log(ordersPlaced)
 
