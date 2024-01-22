4437dc34ccf843126dbcc8e7cad1a6e37ae07341
joeyissa <joeyissa59@gmail.com>
2024-01-21 22:42:56 -0500

    adding comments

--- slice-of-pi-dashboard/src/components/MoneyChart.js
+++ slice-of-pi-dashboard/src/components/MoneyChart.js
@@ -29,11 +29,14 @@ const MoneyChart = () => {
         });
     });
 
+    // Colors sourced from ChatGPT becuase I am tired
+
     const getBorderColor = (index) => {
         const colors = ['rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'];
         return colors[index % colors.length];
     };
-
+    
+    // Colors sourced from ChatGPT becuase I am tired
     const getBackgroundColor = (index) => {
         const colors = ['rgba(54, 162, 235, 0.4)', 'rgba(255, 206, 86, 0.4)', 'rgba(75, 192, 192, 0.4)', 'rgba(153, 102, 255, 0.4)', 'rgba(255, 159, 64, 0.4)'];
         return colors[index % colors.length];
--- slice-of-pi-dashboard/src/components/TotalDisplay.js
+++ slice-of-pi-dashboard/src/components/TotalDisplay.js
@@ -8,28 +8,37 @@ import CountUp  from 'react-countup';
 
 const TotalDisplay = () => {
 
-    // gandTotal, KanataTotal, OrleansTotal, SandyHillTotal, TheGlebe
+    // gandTotal, KanataTotal, OrleansTotal, SandyHillTotal, TheGlebe -- easy to copy and paste to keep track of places
     const ordersPlaced = {};
     const moneyMade = {};
     let grandTotal = 0;
 
     OrderData.forEach((order) => {  
         const store = order.store; 
-        order.items.forEach((items) => {
+        order.items.forEach((items) => {  
             const type = items.type;
             const size = items.size;
             ordersPlaced[store] = ordersPlaced[store] || {};
             ordersPlaced[store][type] = ordersPlaced[store][type] || {};
-            ordersPlaced[store][type][size] = (ordersPlaced[store][type][size] || 0) + 1; // set the value or initalize
+            ordersPlaced[store][type][size] = (ordersPlaced[store][type][size] || 0) + 1; // initalize values so i dont get errors 
+            // basically get the # of pizzas ordered by each type and size
+            /*
+              Kanata: {
+                Cheese: {
+                    S: 5,
+                    M: 8,
+                    L: 3
+                },
+            */
 
-            moneyMade[store] = moneyMade[store] || 0; // set a standard count
-            moneyMade[store] += PricingData[type][size];
+            moneyMade[store] = moneyMade[store] || 0; // set a base coutnt
+            moneyMade[store] += PricingData[type][size]; // access price of each directly from json
             grandTotal += PricingData[type][size];
 
         });
     });
 
-    /*
+    /* did not even use
     const data = {
         // i want this to auto generate the couuptoggle depending on the # of keys in the dictionary
     }
